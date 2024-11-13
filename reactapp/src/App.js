356f793aced8834aabb338e0d8ab6ae802410208
Rathoreshivani <shivanirathore1077@gmail.com>
2024-11-13 17:17:09 +0530

    added

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,20 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './component/Login';
 import Registration from './component/Registration';
-import { BrowserRouter,Router,Route, Routes } from "react-router-dom";  //dynamic browserrouter
+import { BrowserRouter, Route, Routes } from "react-router-dom";  // dynamic BrowserRouter
+
 function App() {
   return (
-   // <Login/>
-   <>
-   <BrowserRouter>
-        <Routes>   //completely broweser per depend he
-        <Route path='/Login' element={<Login/>} />
-          <Route path='/Registration' element={<Registration/>} />
+    // <Login/>
+    <>
+      <BrowserRouter>
+        <Routes>   {/* completely browser dependent */}
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Registration' element={<Registration />} />
         </Routes>
-        </BrowserRouter>
+      </BrowserRouter>
     </>
-    
   );
 }
 
 export default App;
+
